3aefb5538ad99b75bb64d311eb0d745f1c48ba74
Maurice de Beijer <maurice.de.beijer@gmail.com>
2018-10-29 17:23:05 +0000

    React Router Dom demo

--- mod-03/src/App.js
+++ mod-03/src/App.js
@@ -1,20 +1,36 @@
 import React, { Component } from 'react';
+
+import { BrowserRouter, Route, Link, Switch, Redirect } from 'react-router-dom'
+
 import './App.css';
 
 import Cat from './components/Cat'
 import Dog from './components/Dog'
+import NotFound from './components/NotFound'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header >
-          <a href="#">Cats</a>
-          &nbsp;
-          <a href="#">Dogs</a>
-        </header>
-        <Cat />
-        <Dog />
+        <BrowserRouter>
+          <>
+            <header >
+              <Link to="/">Home</Link>
+              &nbsp;
+              <Link to="/cats">Cats</Link>
+              &nbsp;
+              <Link to="/dog">Dog</Link>
+            </header>
+            <Switch>
+              <Route path='/cats/:name' exact component={Cat} />
+              <Route path='/cats' exact component={Cat} />
+              <Route path='/dog' component={Dog} />
+              <Route path='/' exact  render={() => <div>We are home</div>} />
+              <Route render={() => <Redirect to='/'/>}/>
+              // <Route component={NotFound} />
+            </Switch>
+          </>
+        </BrowserRouter>
       </div>
     );
   }
