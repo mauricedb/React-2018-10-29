ae167be779f490ac5c2f395e7bfd5f0c8d0df4e1
Maurice de Beijer <maurice.de.beijer@gmail.com>
2018-10-31 16:22:37 +0000

    new TypeScript demo

--- mod-10/src/demo.js
+++ mod-10/src/demo.js
@@ -1,3 +1,12 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 class Calculator {
     constructor(pre, inp) {
         this.pre = pre;
@@ -14,7 +23,9 @@ class Calculator {
         return diff;
     }
     show(result) {
+        // if (this.pre) {
         this.pre.textContent = result.toString();
+        // }
         this.inp.value = result.toString();
     }
     get name() {
@@ -23,23 +34,66 @@ class Calculator {
 }
 const pre = document.getElementById('result1');
 const inp = document.getElementById('result2');
-const calc = new Calculator(pre, inp);
-console.log('1 + 2 =', calc.add(1, 2));
-console.log('50 - 8 =', calc.subtract(50, 8));
+if (pre) {
+    const calc = new Calculator(pre, inp);
+    console.log('1 + 2 =', calc.add(1, 2));
+    console.log('50 - 8 =', calc.subtract(50, 8));
+}
 class Cat {
     eat() {
+        console.log('Cat is eating');
     }
     climb() {
+        console.log('Cat is climbing');
     }
 }
 class Dog {
     eat() {
+        console.log('Dog is eating');
     }
     bark() {
+        console.log('Dog is barking');
     }
 }
 function doStuff(animal) {
+    if (animal) {
+        animal.eat();
+    }
+    if (animal instanceof Dog) {
+        animal.bark();
+    }
+    if (animal instanceof Cat) {
+        animal.climb();
+    }
 }
 doStuff(new Cat());
 doStuff(new Dog());
+doStuff({ eat() { }, bark() { } });
+doStuff(null);
+doStuff(undefined);
+function getData() {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            const rsp = yield fetch('movies.json');
+            if (rsp.ok) {
+                const data = yield rsp.json();
+                return data;
+            }
+        }
+        catch (err) {
+            console.log(err);
+        }
+        return null;
+    });
+}
+getData();
+var zorro = {
+    name: 'Zorro',
+    eat(food) {
+        console.log(this.name, 'is eating', food);
+    }
+};
+zorro.eat('meat');
+const eat = zorro.eat;
+//   eat('')
 //# sourceMappingURL=demo.js.map
--- mod-10/src/demo.ts
+++ mod-10/src/demo.ts
@@ -3,20 +3,22 @@ class Calculator {
     constructor(private pre: HTMLElement, public inp: HTMLInputElement) {
     }
 
-    add(x, y) {
+    add(x: number, y: number) {
         const sum = x + y
         this.show(sum)
         return sum;
     }
 
-    subtract(x, y) {
+    subtract(x: number, y: number) {
         const diff = x - y
         this.show(diff)
         return diff;
     }
 
-    show(result) {
+    show(result: number) {
+        // if (this.pre) {
         this.pre.textContent = result.toString()
+        // }
         this.inp.value = result.toString()
     }
 
@@ -29,29 +31,83 @@ class Calculator {
 
 const pre = document.getElementById('result1')
 const inp = document.getElementById('result2') as HTMLInputElement
-
-const calc = new Calculator(pre, inp)
-console.log('1 + 2 =', calc.add(1, 2));
-console.log('50 - 8 =', calc.subtract(50, 8))
-
+if (pre) {
+    const calc = new Calculator(pre, inp)
+    console.log('1 + 2 =', calc.add(1, 2));
+    console.log('50 - 8 =', calc.subtract(50, 8))
+}
 
 class Cat {
     eat() {
+        console.log('Cat is eating')
     }
     climb() {
+        console.log('Cat is climbing')
     }
 }
 
 class Dog {
     eat() {
+        console.log('Dog is eating')
     }
     bark() {
+        console.log('Dog is barking')
     }
 }
 
-function doStuff(animal) {
+function doStuff(animal: Cat | Dog | null | undefined) {
+    if (animal) {
+        animal.eat()
+    }
 
+    if (animal instanceof Dog) {
+        animal.bark()
+    }
+    if (animal instanceof Cat) {
+        animal.climb()
+    }
 }
 
 doStuff(new Cat())
 doStuff(new Dog())
+
+
+doStuff({ eat() { }, bark() { } })
+
+doStuff(null)
+doStuff(undefined)
+
+async function getData() {
+    try {
+        const rsp = await fetch('movies.json')
+        if (rsp.ok) {
+            const data = await rsp.json()
+
+            return data
+        }
+    }
+    catch (err) {
+        console.log(err)
+    }
+    return null
+}
+
+
+
+
+getData()
+
+
+var zorro = {
+    name:'Zorro',
+    eat(this: {name: string}, food: string) {
+      console.log(
+        this.name, 'is eating', food);
+    }
+  };
+  
+  zorro.eat('meat');
+
+  
+  const eat = zorro.eat
+//   eat('')
